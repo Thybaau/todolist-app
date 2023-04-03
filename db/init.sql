76f642b7540dbffc7484a9c6cfacc6a16ca3290b
Thybaau <thibaultcaremelle@gmail.com>
2023-04-03 22:44:04 +0200

    Create table for tasks

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
-CREATE TABLE tasks(
-    id serial primary key,
-    title,
-    content,
-    date
-)
+CREATE TABLE IF NOT EXISTS tasks(
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100),
+    content VARCHAR(600),
+    statu BOOLEAN NOT NULL
+);
