d64e767f5ba9a53644b7d1c95d19fc4eb29de216
Thybaau <thibaultcaremelle@gmail.com>
2023-12-03 15:36:38 +0100

    Revert "Merge branch 'develop' into feature-server-go"
    This reverts commit 5230dc3b0308f8f6d0be95faee4dc5dbebaadcf1, reversing
    changes made to 0989f336d3b46508298929b18439679897d6a1ba.

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
--- Tasks table creation
 CREATE TABLE IF NOT EXISTS tasks(
-    id SERIAL PRIMARY KEY,
-    content TEXT,
-    state BOOLEAN NOT NULL DEFAULT FALSE
+    id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100),
+    content VARCHAR(600),
+    statu BOOLEAN NOT NULL DEFAULT FALSE
 );
