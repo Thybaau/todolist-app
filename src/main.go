ba26c144b23c0ac6f1f7d3aed6fc20175b65825d
Thybaau <thibaultcaremelle@gmail.com>
2023-12-03 12:53:52 +0100

    Create index endpoint

--- src/main.go
+++ src/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Thybaau/todolist-app/router"
+)
+
+func main() {
+	log.Printf("Todo-list app Golang")
+	r := router.Router()
+
+	// Server connexion
+	// http.HandleFunc("/", srv.serveHTTP)
+	log.Printf("Serving HTTP on port 9000")
+	err := http.ListenAndServe(":9000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
