23e8566eec4208d9785d62074951980649c70279
Thybaau <thibaultcaremelle@gmail.com>
2023-12-03 15:15:10 +0100

    Fix column id by removing "not null" in init.sql
    SERIAL already has function not null, so it was useless

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 -- Tasks table creation
 CREATE TABLE IF NOT EXISTS tasks(
-    id SERIAL PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     content TEXT,
     state BOOLEAN NOT NULL DEFAULT FALSE
 );
