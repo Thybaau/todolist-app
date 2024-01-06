62319fa8b43b07ca4f9bf1bd07caa057a7bff25d
Thybaau <thibaultcaremelle@gmail.com>
2024-01-06 17:24:33 +0100

    Create functions to encode error et response in json format in requests

---
+++ src/middleware/response.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type HTTPError struct {
+	Error  string `json:"error"`
+	Detail string `json:"detail"`
+}
+
+func NewHTTPError(w http.ResponseWriter, message string, status int, err error) {
+	resp := HTTPError{
+		Error:  message,
+		Detail: err.Error(),
+	}
+	JSONResponse(w, status, resp)
+}
+
+func JSONResponse(w http.ResponseWriter, status int, content interface{}) {
+	resp, err := json.Marshal(content)
+	if err != nil {
+		log.Printf("Cannot encode json, err =%v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(resp)
+}
