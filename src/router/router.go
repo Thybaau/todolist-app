fad38234520873312c405282fe516372f9e87687
Thybaau <thibaultcaremelle@gmail.com>
2024-01-07 18:44:35 +0100

    Update endpoints adress

--- src/router/router.go
+++ src/router/router.go
@@ -2,8 +2,8 @@ package router
 
 func (s *server) router() {
 	s.Router.HandleFunc("/", s.handleIndex()).Methods("GET")
-	s.Router.HandleFunc("/tasks/list/", s.handleTaskList()).Methods("GET")
-	s.Router.HandleFunc("/tasks/create/", s.handleTaskCreate()).Methods("POST")
+	s.Router.HandleFunc("/tasks", s.handleTaskList()).Methods("GET")
+	s.Router.HandleFunc("/tasks", s.handleTaskCreate()).Methods("POST")
 	s.Router.HandleFunc("/tasks/{id:[0-9]+}", s.handleTaskDelete()).Methods("DELETE")
 	s.Router.HandleFunc("/tasks/{id:[0-9]+}", s.handleTaskEdit()).Methods("PUT")
 }
