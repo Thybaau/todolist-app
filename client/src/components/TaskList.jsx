144bd0b393e1c529b289960e4bbfb1ecc092da87
Thybaau <thibaultcaremelle@gmail.com>
2024-02-15 20:00:53 +0100

    Add task list in react

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -9,13 +9,14 @@ export default function TaskList() {
         })
             .then(response => response.json())
             .then(data => setTasks(data))
-            .catch(error => console.error('Erreur lors de la récupération des tâches', error));
+            .catch(error => console.error('Error while getting tasks', error));
     }, [])
     return (
         <ul>
             {tasks.map(task => (
-                <li key={task.id}>
-                    {task.content} - État : {task.state}
+                <li key={task.id} className="p-2 bg-zinc-200 mb-2 rounded flex">
+                    {task.content}
+                    <button className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">X</button>
                 </li>
                 ))}
         </ul>
