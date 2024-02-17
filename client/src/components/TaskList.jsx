3d61de80702b7f29901d25cb2bd8be8d32c34f03
Thybaau <thibaultcaremelle@gmail.com>
2024-02-18 00:01:19 +0100

    Add delete of task with button

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -11,12 +11,34 @@ export default function TaskList() {
             .then(data => setTasks(data))
             .catch(error => console.error('Error while getting tasks', error));
     }, [])
+
+    function deleteTask(id){
+        fetch(`http://localhost:8080/tasks/${id}`, {
+            method: 'DELETE'
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(errorData => {
+                    const errorMessage = errorData.detail || errorData.error || 'Unknown error occurred';
+                    throw new Error(`HTTP error status: ${response.status}, Message: ${errorMessage}`);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            setTasks(tasks.filter(task => task.id !== id));
+        })
+        .catch(error => {
+            console.error('Error while deleting task : ', error.message);
+        });
+    }
     return (
         <ul>
             {tasks.map(task => (
                 <li key={task.id} className="p-2 bg-zinc-200 mb-2 rounded flex">
                     {task.content}
-                    <button className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">X</button>
+                    <button onClick={() => deleteTask(task.id)} className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">X</button>
                 </li>
                 ))}
         </ul>
