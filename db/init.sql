5adf951b1f23cf519f2da0c5261d08748c8c73f6
Thybaau <thibaultcaremelle@gmail.com>
2024-05-04 11:46:58 +0200

    Add users table in database

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,13 @@
+-- Create Users table for authentication
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
 --Create Tasks table
 CREATE TABLE IF NOT EXISTS tasks(
     id SERIAL PRIMARY KEY,
     content TEXT,
-    state BOOLEAN NOT NULL DEFAULT FALSE
+    state BOOLEAN NOT NULL DEFAULT FALSE,
+    username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE
 );
