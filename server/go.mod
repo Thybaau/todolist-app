1dcc3b00d6ce09dccf3d0d9b045c4de44574845b
Thybaau <thibaultcaremelle@gmail.com>
2024-05-04 11:48:00 +0200

    Add JWT middleware

--- server/go.mod
+++ server/go.mod
@@ -12,8 +12,10 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.22.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ server/middleware/token.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var secretKey = []byte("secretpassword")
+
+func GenerateToken(username string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token valid for 24 hours
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Check the signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("invalid signing method")
+		}
+		return secretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate the token
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("invalid token")
+}
+
+func JWTMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Récupérer le jeton JWT depuis l'en-tête Authorization
+		tokenString := r.Header.Get("Authorization")
+		if tokenString == "" {
+			NewHTTPError(w, "Missing authorization token", http.StatusUnauthorized, nil)
+			return
+		}
+
+		claims, err := VerifyToken(tokenString)
+		if err != nil {
+			NewHTTPError(w, "Invalid authentication token", http.StatusUnauthorized, err)
+			return
+		}
+
+		username, ok := claims["username"]
+		if !ok {
+			NewHTTPError(w, "Username not found in token payload", http.StatusUnauthorized, nil)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "username", username)
+		r = r.WithContext((ctx))
+		next.ServeHTTP(w, r)
+	})
+}
